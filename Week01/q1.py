0caec731dfeb73b20dc26798677e524e4f7a1f38
syed97 <43263499+syed97@users.noreply.github.com>
2018-09-14 12:56:03 +0500

    Week 1
    Q1 and Q2 Solutions

---
+++ Week01/q1.py
@@ -0,0 +1,26 @@
+#Problem 1
+condition = "y"
+strings = [];
+while (condition == "y"):
+    value = input("Enter your string. Input n to discontinue")
+    if (value == "n"):
+        condition = "n"
+    else:
+        strings.append(value)
+size = len(strings)
+
+max_string = 0
+for i in strings:
+    if len(i) > max_string:
+        max_string = len(i)
+asterisks = "* "*(max_string)
+print(asterisks)
+for i in strings:
+    print("* " + i + (len(asterisks) - len(i)-4)*" "+"*")
+print(asterisks)
+
+
+#Problem 2
+
+    
+        
